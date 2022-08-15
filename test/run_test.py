473f6ef603dd30e1b8d7d4ab59b6b7068f15ec5c
Catherine Lee <csl@fb.com>
2022-08-15 09:36:07 -0700

    a

--- test/run_test.py
+++ test/run_test.py
@@ -56,7 +56,7 @@ def is_special_file(file: str) -> bool:
     else:
         return file in ['test_nn', 'test_fake_tensor', 'test_cpp_api_parity', 'test_jit_cuda_fuser', 'test_reductions',
                         'test_cuda', 'test_indexing', 'test_fx_backends', 'test_linalg', 'test_cpp_extensions_jit',
-                        'test_torch', 'test_tensor_creation_ops', 'test_sparse_csr']
+                        'test_torch', 'test_tensor_creation_ops', 'test_sparse_csr', 'test_dispatch']
 
 
 def discover_tests(
