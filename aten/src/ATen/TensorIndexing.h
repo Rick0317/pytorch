70d597b408997f83b4609a438733ce4aef38d2e9
Horace He <chilli@fb.com>
2022-08-31 01:17:31 +0000

    Remove .item calls from indexing
    ghstack-source-id: 1cfed1d8c47256bc67b2fb675044a534d751b372
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84159

--- aten/src/ATen/TensorIndexing.h
+++ aten/src/ATen/TensorIndexing.h
@@ -465,38 +465,29 @@ static inline Tensor handleDimInMultiDimIndexing(
     const Tensor& tensor = index.tensor();
     auto scalar_type = tensor.scalar_type();
     if (tensor.dim() == 0 &&
-        at::isIntegralType(scalar_type, /*includeBool=*/true)) {
-      if (scalar_type != at::kByte && scalar_type != at::kBool) {
-        result = impl::applySelect(
-            result,
-            *dim_ptr,
-            tensor.item<int64_t>(),
-            real_dim,
-            original_tensor_device,
-            prev_dim_result_sizes);
+        (scalar_type == at::kByte || scalar_type == at::kBool)) {
+      result = result.unsqueeze(*dim_ptr);
+      if (scalar_type == at::kBool) {
+        impl::recordTensorIndex(
+            impl::boolToIndexingTensor(
+                result, tensor.item<bool>() != 0, original_tensor_device),
+            outIndices,
+            dim_ptr);
       } else {
-        result = result.unsqueeze(*dim_ptr);
-        if (scalar_type == at::kBool) {
-          impl::recordTensorIndex(
-              impl::boolToIndexingTensor(
-                  result, tensor.item<bool>() != 0, original_tensor_device),
-              outIndices,
-              dim_ptr);
-        } else {
-          impl::recordTensorIndex(
-              impl::boolToIndexingTensor(
-                  result, tensor.item<uint8_t>() != 0, original_tensor_device),
-              outIndices,
-              dim_ptr);
-        }
+        impl::recordTensorIndex(
+            impl::boolToIndexingTensor(
+                result, tensor.item<uint8_t>() != 0, original_tensor_device),
+            outIndices,
+            dim_ptr);
       }
-    } else {
-      impl::recordTensorIndex(tensor, outIndices, dim_ptr);
     }
-    return result;
-  } else {
-    TORCH_INTERNAL_ASSERT(false, "Invalid TensorIndex type");
   }
+  impl::recordTensorIndex(tensor, outIndices, dim_ptr);
+  return result;
+}
+else {
+  TORCH_INTERNAL_ASSERT(false, "Invalid TensorIndex type");
+}
 }
 
 namespace impl {
