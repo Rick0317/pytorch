a94331888171d7d7bdf73343d8a01b99db82350e
Sahan Paliskara <sahancpal@gmail.com>
2022-10-06 12:57:19 -0700

    Add torch::deploy tests to CI
    ghstack-source-id: 8f6b4490e6ca64590d0d5d55aeda69b71b56838c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/86155

--- .jenkins/pytorch/common_utils.sh
+++ .jenkins/pytorch/common_utils.sh
@@ -111,6 +111,36 @@ function checkout_install_torchvision() {
   popd
 }
 
+function checkout_install_torchdeploy() {
+  local commit
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl gnupg lsb-release
+   sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  python setup.py bdist_wheel
+  pushd ..
+  git clone https://github.com/pytorch/multipy
+  pushd multipy
+  git checkout 850862ddc5f5f614697b2415e42df982f04df3ed
+  mkdir temp
+  popd
+  popd
+  # currently in /var/lib/jenkins/workspace
+  pwd
+  ls
+  cp -r dist/ ../multipy/temp/
+  pushd ..
+  pushd multipy
+  DOCKER_BUILDKIT=1 docker build -t multipy --progress=plain --build-arg PYTHON_MINOR_VERSION=10 .
+  popd
+  popd
+}
+
 function clone_pytorch_xla() {
   if [[ ! -d ./xla ]]; then
     git clone --recursive --quiet https://github.com/pytorch/xla.git
